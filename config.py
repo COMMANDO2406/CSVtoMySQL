d1ec276bf4d69f2374ef2f1048bb4372484ea14b
COMMANDO2406 <raghavsethi.in@gmail.com>
2024-07-04 21:48:39 +0530

    improved code comments and fixed database exist error

---
+++ config.py
@@ -0,0 +1,8 @@
+# Enter host for your mysql server here, the default is set to localhost replace it if required
+host = "localhost"
+
+# Enter username for your mysql server
+user = "USERNAME"
+
+# Enter password for your mysql server
+password = "PASSWORD"
